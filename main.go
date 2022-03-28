d99941acc17319ce8be2a823dd80309dfdaa83cd
Kael <89662068+EkkoSummer@users.noreply.github.com>
2022-03-28 10:11:47 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Counter struct {
+	//线程安全：并发时只读
+	M *sync.Map
+}
+
+func (c *Counter) Incr(s interface{}, n int) {
+	//成功读取则累加
+	if addend, loadSuccess := c.M.LoadOrStore(s, n); loadSuccess {
+		c.M.Store(s, addend.(int)+n)
+	}
+}
+
+func (c *Counter) Init() {
+	*c = Counter{new(sync.Map)}
+}
+
+//获取数据，如果成功则返回累加结果
+func (c *Counter) Get(s interface{}) int {
+	addend, loadSuccess := c.M.Load(s)
+	if loadSuccess {
+		return addend.(int)
+	} else {
+		return 0
+	}
+}
+
+func (c *Counter) Flush2Broker(ms int, FuncCbFlush func()) {
+	go func() {
+		//类型转换：int->时间类型（毫秒）
+		ticker := time.NewTicker(time.Millisecond * time.Duration(ms))
+		//异步
+		go func() {
+			for range ticker.C {
+				//每5秒重置一次计数器，一秒一次
+				fmt.Printf("每%.2fs重置计数器\n", float64(ms/1000))
+				FuncCbFlush()
+			}
+		}()
+	}()
+}
+
+// 重置计数器
+func (c *Counter) FuncCbFlush() {
+	c.Init()
+}
+
+func main() {
+	counter := &Counter{}
+	counter.Init()
+	counter.Flush2Broker(5000, counter.FuncCbFlush)
+	//counter.Incr("get.called", 123)
+	//counter.Incr("get.called", 456)
+
+	for i := 0; i < 12; i++ {
+		counter.Incr("get.called", 1)
+		fmt.Println(counter.Get("get.called"))
+		time.Sleep(1 * time.Second)
+	}
+}
